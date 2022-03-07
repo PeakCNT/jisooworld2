c97440cdb9897423b90afdd1a8f9ebd3893f1549
chanatip payomyam <chanatip-ninenine@gmail.com>
2022-03-08 00:30:31 +0700

    fix pic

--- src/components/Gallery/Photo.jsx
+++ src/components/Gallery/Photo.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import "../../index.css";
-import 'antd/dist/antd.css';
-import { Image } from 'antd';
+import "antd/dist/antd.css";
+import { Image } from "antd";
 
 const Photo = ({ id, name, content, like, img }) => {
   return (
     <article className="photo">
       <Image
-      className="column-img"
-      style={{'width':'33vw'}}
-      src={img}
-      alt={name}
-    />
+        className="column-img"
+        style={{ width: "100vw" }}
+        src={img}
+        alt={name}
+      />
       <div className="photo-info">
         <h2>{name}</h2>
         <p>{like} likes</p>
